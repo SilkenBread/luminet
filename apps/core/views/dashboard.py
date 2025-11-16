700e49228dc2e8b4b825a3ad8e4140cf9e91ff7b
Juan Rubio <jua0729.jr@gmail.com>
2025-11-16 18:22:23 -0500

    [ADD] User dashboard counter

--- apps/core/views/dashboard.py
+++ apps/core/views/dashboard.py
@@ -4,10 +4,16 @@ from django.urls import reverse_lazy
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard.html'
+
+    def get_active_users(self):
+        from apps.users.models import User
+        return User.objects.filter(is_active=True).count()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Dashboard - Luminet'
         context['list_url'] = reverse_lazy('core:dashboard')
         context['show_sidebar'] = True
+        context['module'] = 'Dashboard'
+        context['active_users_count'] = self.get_active_users()
         return context
